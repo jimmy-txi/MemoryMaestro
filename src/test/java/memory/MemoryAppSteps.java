0e9a16a37a25c5b22d7bba06602b8b3f6476ee0a
jimmy <46294820+jimmy-txi@users.noreply.github.com>
2025-01-08 11:26:52 +0100

    Update MemoryAppSteps.java

--- src/test/java/memory/MemoryAppSteps.java
+++ src/test/java/memory/MemoryAppSteps.java
@@ -18,7 +18,7 @@ public class MemoryAppSteps {
 
     @Then("le nombre total de cartes doit être {int}")
     public void leNombreTotalDeCartesDoitEtre(int nbCartes) {
-        assertEquals(nbCartes, 19000);
+        assertEquals(nbCartes, jeu.getNbCartes());
     }
 
     @Then("aucune carte ne doit être trouvée")
